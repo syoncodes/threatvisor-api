8a6502849fb7d19f437dc38f3d02e2897db872e3
syoncodes <34947109+syoncodes@users.noreply.github.com>
2024-06-12 22:03:49 -0500

    Update general.js

--- routes/general.js
+++ routes/general.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
-const axios = require("axios");
+
 
 const newsSchema = new mongoose.Schema({
   title: String,
   description: String,
   date: String,
-  coverUrl: String,
+  coverUrl: String, // Assuming this is a URL to an image in Google Drive
   new: Boolean,
   head1: String,
   head2: String,
@@ -15,48 +14,18 @@ const newsSchema = new mongoose.Schema({
   article1: String,
   article2: String,
   article3: String,
-  img1URL: String,
-  img2URL: String,
+  img1URL: String, // Assuming this is a URL to an image in Google Drive
+  img1URL: String, // Assuming this is a URL to an image in Google Drive
 });
 
 const News = mongoose.model('News', newsSchema);
 
-const getGoogleDriveFileId = (url) => {
-  const match = url.match(/\/d\/(.*?)\//);
-  return match ? match[1] : null;
-};
-
-const fetchImage = async (url) => {
-  const fileId = getGoogleDriveFileId(url);
-  if (!fileId) {
-    throw new Error('Invalid Google Drive URL');
-  }
-
-  const googleDriveUrl = `https://drive.google.com/uc?export=view&id=${fileId}`;
-  try {
-    const response = await axios.get(googleDriveUrl, {
-      responseType: 'arraybuffer',
-    });
-    const base64Image = Buffer.from(response.data, 'binary').toString('base64');
-    const contentType = response.headers['content-type'];
-    return `data:${contentType};base64,${base64Image}`;
-  } catch (error) {
-    throw new Error('Error fetching image from Google Drive');
-  }
-};
+module.exports = News;
 
 router.get('/featured', async (req, res) => {
   try {
     const featuredNews = await News.find({});
-    const posts = await Promise.all(
-      featuredNews.map(async (post) => {
-        const coverUrl = await fetchImage(post.coverUrl);
-        const img1URL = await fetchImage(post.img1URL);
-        const img2URL = await fetchImage(post.img2URL);
-        return { ...post.toObject(), coverUrl, img1URL, img2URL };
-      })
-    );
-    res.json(posts);
+    res.json(featuredNews);
   } catch (error) {
     res.status(500).send('Error fetching featured news');
   }
